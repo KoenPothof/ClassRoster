780c05f822071f9185e466c3b2a155f7212c04ab
KoenPothof <koenpothof@gmail.com>
2023-02-09 15:52:41 +0100

    GUIboxen update2

--- src/GUI/GUIboxrooster.java
+++ src/GUI/GUIboxrooster.java
@@ -1,2 +1,143 @@
-package GUI;public class GUIboxrooster {
+package GUI;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class GUIboxrooster extends Application {
+    public GUIrooster guirooster = new GUIrooster();
+    private String value1;
+    private String value2;
+    private String value3;
+    private String value4;
+
+    @Override
+    public void start(Stage stage) {
+        Label time = new Label("8.30-9.20");
+        Label time1 = new Label("9.20-10.10");
+        Label time2 = new Label("10.10-11.00");
+        Label time3 = new Label("11.00-11.20");
+        Label subject3 = new Label("Pauze");
+        Label time4 = new Label("11.20-12.10");
+        Label time5 = new Label("12.10-13.00");
+        Label time6 = new Label("13.00-13.20");
+        Label subject6 = new Label("Pauze");
+        Label time7 = new Label("13.20-14.10");
+        Label time8 = new Label("14.10-15.00");
+        ArrayList<Label> labels = new ArrayList<>();
+        labels.add(time);
+        labels.add(time1);
+        labels.add(time2);
+        labels.add(time3);
+        labels.add(subject3);
+        labels.add(time4);
+        labels.add(time5);
+        labels.add(time6);
+        labels.add(subject6);
+        labels.add(time7);
+        labels.add(time8);
+
+        Button button = new Button("sex");
+
+        button.setOnAction(e -> {
+              guirooster.start(stage);
+        });
+
+
+
+        ComboBox subjectBox = new ComboBox();
+        subjectBox.getItems().add("IPJ");
+        subjectBox.getItems().add("Wiskunde");
+        subjectBox.getItems().add("Proftaak");
+
+        subjectBox.setOnAction(e -> {
+            value1 = (String) subjectBox.getValue();
+            if(value1 !=null) {
+                if(value1.equals("Wiskunde")){
+                    Label Wiskunde = new Label("Wiskunde");
+                    guirooster.newLabel(guirooster.subject, Wiskunde);
+                }else if(value1.equals("IPJ")){
+                    Label IPJ = new Label("IPJ");
+                    guirooster.newLabel(guirooster.subject, IPJ);
+                }else if(value1.equals("Proftaak")){
+                    Label Proftaak = new Label("Proftaak");
+                    guirooster.newLabel(guirooster.subject, Proftaak);
+                }
+            }
+        });
+
+        ComboBox teacherBox = new ComboBox();
+        teacherBox.getItems().add("Joehan");
+        teacherBox.getItems().add("Joelie");
+        teacherBox.getItems().add("Robijn");
+        teacherBox.getItems().add("Potter");
+        teacherBox.getItems().add("EddLose");
+        teacherBox.getItems().add("Shans");
+
+
+        teacherBox.setOnAction(e -> {
+            value2 = (String) teacherBox.getValue();
+            if(value2 !=null) {
+                System.out.println(value2);
+            }
+        });
+
+        ComboBox classroomBox = new ComboBox();
+        classroomBox.getItems().add("La134");
+        classroomBox.getItems().add("La136");
+        classroomBox.getItems().add("Ld112");
+
+        classroomBox.setOnAction(e -> {
+            value3 = (String) classroomBox.getValue();
+            if(value3 !=null) {
+                System.out.println(value3);
+            }
+        });
+
+        ComboBox classBox = new ComboBox();
+        classBox.getItems().add("A1");
+        classBox.getItems().add("A2");
+        classBox.getItems().add("A3");
+        classBox.getItems().add("A4");
+
+        classBox.setOnAction(e -> {
+            value4 = (String) classBox.getValue();
+            if(value4 !=null) {
+                System.out.println(value4);
+            }
+        });
+
+        HBox hbox = new HBox();
+        hbox.setSpacing(100);
+        VBox vbox1 = new VBox();
+        vbox1.setSpacing(25);
+        for (Label labell:
+                labels) {
+            vbox1.getChildren().add(labell);
+        }
+
+        VBox vbox2 = new VBox();
+        vbox2.getChildren().addAll(subjectBox);
+        VBox vbox3 = new VBox();
+        vbox3.getChildren().add(teacherBox);
+        VBox vbox4 = new VBox();
+        vbox4.getChildren().add(classBox);
+        hbox.getChildren().addAll(vbox1, vbox2, vbox3, vbox4, button);
+
+        Scene scene = new Scene(hbox);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
 }
--- src/GUI/GUIrooster.java
+++ src/GUI/GUIrooster.java
@@ -1,2 +1,190 @@
-package GUI;public class GUIrooster {
+package GUI;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+
+public class GUIrooster extends Application {
+    public Label time = new Label("8.30-9.20");
+    public Label subject = new Label("");
+    public Label teacher = new Label("");
+    public Label location = new Label("");
+    public Label group = new Label("");
+    public Label time1 = new Label("9.20-10.10");
+    public Label subject1 = new Label("");
+    public Label teacher1 = new Label("");
+    public Label location1 = new Label("");
+    public Label group1 = new Label("");
+    public Label time2 = new Label("10.10-11.00");
+    public Label subject2 = new Label("");
+    public Label teacher2 = new Label("");
+    public Label location2 = new Label("");
+    public Label group2 = new Label("");
+    public Label time3 = new Label("11.00-11.20");
+    public Label subject3 = new Label("Pauze");
+    public Label teacher3 = new Label("-");
+    public Label location3 = new Label("-");
+    public Label group3 = new Label("-");
+    public Label time4 = new Label("11.20-12.10");
+    public Label subject4 = new Label("");
+    public Label teacher4 = new Label("");
+    public Label location4 = new Label("");
+    public Label group4 = new Label("");
+    public Label time5 = new Label("12.10-13.00");
+    public Label subject5 = new Label("");
+    public Label teacher5 = new Label("");
+    public Label location5 = new Label("");
+    public Label group5 = new Label("");
+    public Label time6 = new Label("13.00-13.20");
+    public Label subject6 = new Label("Pauze");
+    public Label teacher6 = new Label("-");
+    public Label location6 = new Label("-");
+    public Label group6 = new Label("-");
+    public Label time7 = new Label("13.20-14.10");
+    public Label subject7 = new Label("");
+    public Label teacher7 = new Label("");
+    public Label location7 = new Label("");
+    public Label group7 = new Label("");
+    public Label time8 = new Label("14.10-15.00");
+    public Label subject8 = new Label("");
+    public Label teacher8 = new Label("");
+    public Label location8 = new Label("");
+    public Label group8 = new Label("");
+    public ArrayList<Label> labels = new ArrayList<>();
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Hello world!");
+        VBox vBox = new VBox();
+        Label label1 = new Label("Roosterview");
+        Button addButton = new Button("ADD");
+        Button editButton = new Button("EDIT");
+
+        editButton.setOnAction(e -> {
+
+        });
+
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(label1, addButton, editButton);
+        GridPane lessons = new GridPane();
+
+        Label timeHead = new Label("Time");
+        Label subjectHead = new Label("Subject");
+        Label teacherHead = new Label("Teacher");
+        Label locationHead = new Label("Location");
+        Label groupHead = new Label("group");
+
+        ArrayList<Label> heads = new ArrayList<>();
+        heads.add(timeHead);
+        heads.add(subjectHead);
+        heads.add(teacherHead);
+        heads.add(locationHead);
+        heads.add(groupHead);
+        for (Label label:heads
+        ) {
+            label.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+        }
+        HBox hBox1 = new HBox();
+        hBox1.getChildren().addAll(timeHead, subjectHead, teacherHead, locationHead, groupHead);
+        hBox1.setSpacing(30);
+
+
+        labels.add(time);
+        labels.add(subject);
+        labels.add(teacher);
+        labels.add(location);
+        labels.add(group);
+        labels.add(time1);
+        labels.add(subject1);
+        labels.add(teacher1);
+        labels.add(location1);
+        labels.add(group1);
+        labels.add(time2);
+        labels.add(subject2);
+        labels.add(teacher2);
+        labels.add(location2);
+        labels.add(group2);
+        labels.add(time3);
+        labels.add(subject3);
+        labels.add(teacher3);
+        labels.add(location3);
+        labels.add(group3);
+        labels.add(time4);
+        labels.add(subject4);
+        labels.add(teacher4);
+        labels.add(location4);
+        labels.add(group4);
+        labels.add(time5);
+        labels.add(subject5);
+        labels.add(teacher5);
+        labels.add(location5);
+        labels.add(group5);
+        labels.add(time6);
+        labels.add(subject6);
+        labels.add(teacher6);
+        labels.add(location6);
+        labels.add(group6);
+        labels.add(time7);
+        labels.add(subject7);
+        labels.add(teacher7);
+        labels.add(location7);
+        labels.add(group7);
+        labels.add(time8);
+        labels.add(subject8);
+        labels.add(teacher8);
+        labels.add(location8);
+        labels.add(group8);
+
+        lessons.add(timeHead, 0, 0);
+        lessons.add(subjectHead, 1, 0);
+        lessons.add(teacherHead, 2, 0);
+        lessons.add(locationHead, 3, 0);
+        lessons.add(groupHead, 4, 0);
+
+        int i = 0;
+        int j = 1;
+        boolean k = true;
+        for (Label label: labels
+        ) {
+            lessons.add(label, i, j);
+            i++;
+            if(k){
+                label.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+            }
+            if(i == 5){
+                j++;
+                if(k){
+                    k = false;
+                }
+                else if(!k){
+                    k = true;
+                }
+                i = 0;
+            }
+
+        }
+
+        lessons.setHgap(50);
+        vBox.getChildren().addAll(hBox, hBox1, lessons);
+        Scene scene = new Scene(vBox);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(GUIrooster.class);
+    }
+
+    public void newLabel(Label oldLabel, Label newlabel){
+        if(labels.contains(oldLabel)){
+            int labelIndex =  labels.indexOf(oldLabel);
+            labels.set(labelIndex, newlabel);
+        }
+    }
 }
