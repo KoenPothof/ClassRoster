a5359ee766a44f1aaabc51a76b186dd29d560164
Sander2482039 <sanderderegt@home.nl>
2023-02-16 14:39:04 +0100

    FileOutput methode

---
+++ src/FileOutput.java
@@ -0,0 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class FileOutput {
+
+    public void save(String filename, ArrayList<String> data){  // methode om naar file 
+        try(PrintWriter pw = new PrintWriter(filename)) {
+            for (String s: data) {
+                System.out.printf("Saved: " + s); // debug code
+                pw.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
